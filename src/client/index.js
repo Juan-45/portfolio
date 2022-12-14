782757eac1babc9e00b146ea3ac4720689fed509
Juan-45 <quijuan12@gmail.com>
2022-12-14 16:01:13 -0300

    Update index.js for:
    - Move ThemeProvider to App.js
    - inport font.CSS

--- src/client/index.js
+++ src/client/index.js
@@ -2,19 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import theme from "./theme/theme";
 import App from "./App";
+import "./fonts.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
