04cb1678f9b80d5ff3bae3bf24e5f820fbbcb44d
Juan-45 <quijuan12@gmail.com>
2022-12-14 15:57:47 -0300

    Update webpack settings for:
    - Manege images with assets module
    - Manege fonts files with assets module
    - Add absolute paths for new folders

--- webpack.common.js
+++ webpack.common.js
@@ -3,21 +3,41 @@ const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const moduleObj = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: "babel-loader",
+const commonRule = [
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+    },
+  },
+];
+
+const clientRules = [
+  {
+    test: /\.css$/,
+    use: ["style-loader", "css-loader"],
+  },
+  {
+    test: /\.(png|jp(e*)g|svg)$/,
+    type: "asset/resource",
+    generator: {
+      filename: "assets/images/[name][ext]",
+    },
+    parser: {
+      dataUrlCondition: {
+        maxSize: 8 * 1024, // 8kb
       },
     },
-    {
-      test: /\.css$/,
-      use: ["style-loader", "css-loader"],
+  },
+  {
+    test: /\.(woff|woff2|ttf)$/i,
+    type: "asset/resource",
+    generator: {
+      filename: "assets/fonts/[name][ext]",
     },
-  ],
-};
+  },
+];
 
 const rootProperties = { watch: true };
 
@@ -28,7 +48,9 @@ const client = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist/public"),
   },
-  module: moduleObj,
+  module: {
+    rules: [...commonRule, ...clientRules],
+  },
   plugins: [
     new HtmlWebPackPlugin({ template: "src/client/index.html" }),
     new webpack.ProvidePlugin({
@@ -40,6 +62,8 @@ const client = {
   resolve: {
     alias: {
       components: path.resolve(__dirname, "./src/client/components/"),
+      layout: path.resolve(__dirname, "./src/client/layout/"),
+      assets: path.resolve(__dirname, "./src/client/assets/"),
       theme: path.resolve(__dirname, "./src/client/theme/"),
       hooks: path.resolve(__dirname, "./src/client/hooks/"),
       pages: path.resolve(__dirname, "./src/client/pages/"),
@@ -56,7 +80,7 @@ const server = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
-  module: moduleObj,
+  module: { rules: commonRule },
   externals: [nodeExternals()],
   ...rootProperties,
 };
