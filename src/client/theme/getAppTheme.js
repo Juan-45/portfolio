3c8b0053831da781fce4b91b32e7ff3ab88f09b4
Juan-45 <quijuan12@gmail.com>
2022-12-14 16:03:36 -0300

    Add getAppTheme function:
    - Replace simple theme file
    - Manage breakpoints, touchScreen styles, landscape and portrait styles

--- src/client/theme/getAppTheme.js
+++ src/client/theme/getAppTheme.js
@@ -15,75 +15,186 @@ const common = {
   },
 };
 
-const breakpoints = {
-  touchScreenPortrait: {
+const touchScreensBreakpoints = {
+  portrait: {
     breakpoints: {
       values: {
-        mobile: 429,
-        tablets: 801,
         desktop: 1281,
+        tablets: 801,
+        mobile: 429,
       },
     },
   },
-  touchScreenLandscape: {
+  landscape: {
     breakpoints: {
       values: {
-        mobile: 927,
+        desktop: 1281,
         tablets: 1281,
-      },
-    },
-  },
-  desktopNoTouch: {
-    breakpoints: {
-      values: {
-        xs: 600,
-        sm: 1281,
+        mobile: 927,
       },
     },
   },
 };
 
-const theme = {
-  touchScreenPortrait: createTheme({
-    ...common,
-    ...breakpoints.touchScreenPortrait,
-  }),
-  touchScreenLandscape: createTheme({
-    ...common,
-    ...breakpoints.touchScreenLandscape,
-  }),
+const desktopBreakpoints = {
+  breakpoints: {
+    values: {
+      md: 1281,
+      sm: 1024,
+      xs: 640,
+    },
+  },
+};
 
-  desktopNoTouch: createTheme({
+const touchScreenTheme = {
+  portrait: createTheme({
     ...common,
-    ...breakpoints.desktopNoTouch,
+    ...touchScreensBreakpoints.portrait,
+  }),
+  landscape: createTheme({
+    ...common,
+    ...touchScreensBreakpoints.landscape,
   }),
 };
 
-const devicesStyles = (theme) => ({
-  touchScreenPortrait: {
-    typography: {
-      h3: {
-        [theme.breakpoints.down("mobile")]: {},
-      },
+const desktopTheme = createTheme({
+  ...common,
+  ...desktopBreakpoints,
+});
+
+const getTouchScreenTypography = (theme) => ({
+  fontSize: 16,
+  h1: {
+    lineHeight: "1.5",
+    [theme.breakpoints.up("desktop")]: {
+      fontSize: "2.3rem",
+    },
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "2.3rem",
+    },
+    [theme.breakpoints.down("tablets")]: {
+      fontSize: "2rem",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "1.7rem",
     },
   },
-
-  touchScreenLandscape: {
-    typography: {
-      h3: {
-        [theme.breakpoints.down("mobile")]: {},
-      },
+  h2: {
+    [theme.breakpoints.up("desktop")]: {
+      fontSize: "2.1rem",
+    },
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "2.1rem",
+    },
+    [theme.breakpoints.down("tablets")]: {
+      fontSize: "1.8rem",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "1.5rem",
     },
   },
-
-  desktopNoTouch: {
-    typography: {
-      h3: {
-        [theme.breakpoints.down("xs")]: {},
-      },
+  subtitle1: {
+    lineHeight: "1.2",
+    [theme.breakpoints.up("desktop")]: {
+      fontSize: "1.8rem",
+    },
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "1.8rem",
+    },
+    [theme.breakpoints.down("tablets")]: {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "1.2rem",
+    },
+  },
+  body: {
+    lineHeight: "1.2",
+    [theme.breakpoints.up("desktop")]: {
+      fontSize: "1.6rem",
+    },
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "1.6rem",
+    },
+    [theme.breakpoints.down("tablets")]: {
+      fontSize: "1.3rem",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "1rem",
+    },
+  },
+  button: {
+    lineHeight: "1.2",
+    [theme.breakpoints.up("desktop")]: {
+      fontSize: "1.6rem",
+    },
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "1.6rem",
+    },
+    [theme.breakpoints.down("tablets")]: {
+      fontSize: "1.3rem",
+    },
+    [theme.breakpoints.down("mobile")]: {
+      fontSize: "1rem",
+    },
+  },
+  caption: {
+    lineHeight: "1.2",
+    [theme.breakpoints.up("desktop")]: {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.down("desktop")]: {
+      fontSize: "1.5rem",
+    },
+    [theme.breakpoints.between("tablets")]: {
+      fontSize: "1.2rem",
+    },
+    [theme.breakpoints.between("mobile")]: {
+      fontSize: "0.9rem",
     },
   },
 });
+
+const desktopTypography = {
+  fontSize: 16,
+  h1: {
+    fontSize: "3rem",
+    lineHeight: "1.5",
+  },
+  h2: {
+    fontSize: "2.4rem",
+  },
+  subtitle1: {
+    fontSize: "1.3rem",
+    lineHeight: "1.2",
+  },
+  body: {
+    fontSize: "1rem",
+    lineHeight: "1.2",
+  },
+  button: {
+    fontSize: "1rem",
+    lineHeight: "1.2",
+  },
+  caption: {
+    fontSize: "0.9rem",
+    lineHeight: "1.2",
+  },
+};
+
+const getTouchScreenStyles = (theme) => ({
+  portrait: {
+    typography: getTouchScreenTypography(theme),
+  },
+
+  landscape: {
+    typography: getTouchScreenTypography(theme),
+  },
+});
+
+const desktopStyle = {
+  typography: desktopTypography,
+};
 
 const getAppTheme = () => {
   const { isOnLandScape } = useCheckOrientation();
@@ -93,23 +204,24 @@ const getAppTheme = () => {
     if (isTouchScreen) {
       if (isOnLandScape) {
         console.log("TOUCH DEVICE - LANDSCAPE");
-        return createTheme(
-          theme.touchScreenLandscape,
-          devicesStyles(theme.touchScreenLandscape).touchScreenLandscape
+        const landscape = createTheme(
+          touchScreenTheme.landscape,
+          getTouchScreenStyles(touchScreenTheme.landscape).landscape
         );
+        console.log(landscape);
+        return landscape;
       } else {
         console.log("TOUCH DEVICE - PORTRAIT");
-        return createTheme(
-          theme.touchScreenPortrait,
-          devicesStyles(theme.touchScreenPortrait).touchScreenPortrait
+        const portrait = createTheme(
+          touchScreenTheme.portrait,
+          getTouchScreenStyles(touchScreenTheme.portrait).portrait
         );
+        console.log(portrait);
+        return portrait;
       }
     } else {
       console.log("DESKTOP");
-      return createTheme(
-        theme.desktopNoTouch,
-        devicesStyles(theme.desktopNoTouch).desktopNoTouch
-      );
+      return createTheme(desktopTheme, desktopStyle);
     }
   };
 
