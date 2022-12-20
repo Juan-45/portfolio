91e121a7172058fb7a578ded18f2561ca131dda1
Juan-45 <quijuan12@gmail.com>
2022-12-19 22:32:28 -0300

    Update Welcome component

--- src/client/components/Welcome.js
+++ src/client/components/Welcome.js
@@ -1,47 +1,29 @@
-import Container from "layout/welcome/Container";
-import { Typography, Box, Button } from "@mui/material";
+import Background from "layout/Background";
+import FadeInQuote from "layout/welcome/FadeInQuote";
+import WelcomeButton from "layout/welcome/WelcomeButton";
+import FlexColumn from "layout/FlexColumn";
+import background from "assets/images/welcome/background.jpg";
+import { useState } from "react";
 
 const Welcome = ({ children }) => {
-  return (
-    <>
-      <Container>
-        <Typography
-          variant='h1'
-          sx={{ fontWeight: "600", fontFamily: "Kaushan", width: "85%" }}
-        >
-          "Hay solo dos grandes equivocaciones que se pueden cometer en el
-          camino para lograr la maestria de uno mismo, no comenzar y no ir todo
-          el camino."
-        </Typography>
-        <Typography variant='h2'>
-          Test h2: Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun
-        </Typography>
-        <Typography variant='subtitle1'>
-          Test subtitle1: Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun
-        </Typography>
-        <Typography variant='body'>
-          Test body: Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun
-        </Typography>
-        <Typography variant='button'>
-          Test button: Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun
-        </Typography>
-        <Typography variant='caption'>
-          Test caption: Lorem ipsun Lorem ipsun Lorem ipsun Lorem ipsun
-        </Typography>
-        <Button
-          variant='contained'
-          sx={{
-            // mt: "150px",
-            width: { xs: "150px", sm: "300px" },
-            //fontSize: "1.5rem",
-          }}
-        >
-          Ingresar
-        </Button>
-      </Container>
-      {children}
-    </>
-  );
+  const [show, setShow] = useState(false);
+
+  if (show) {
+    return <>{children}</>;
+  } else {
+    return (
+      <Background background={background}>
+        <FlexColumn variant='welcome'>
+          <FadeInQuote
+            text='"Hay solo dos grandes equivocaciones que se pueden cometer en el
+            camino para lograr la maestria de uno mismo, no comenzar y no ir todo
+            el camino."'
+          />
+          <WelcomeButton onClick={() => setShow(!show)}>Ingresar</WelcomeButton>
+        </FlexColumn>
+      </Background>
+    );
+  }
 };
 
 export default Welcome;
