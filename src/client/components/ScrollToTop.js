246e8e1f73824398ca4a4db6dfc46c7ac19eae9b
Juan-45 <quijuan12@gmail.com>
2023-01-07 15:57:15 -0300

    Add scroll to top component

--- src/client/components/ScrollToTop.js
+++ src/client/components/ScrollToTop.js
@@ -5,8 +5,9 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // window.scrollTo(0, 0);
+    window.history.scrollRestoration = "manual";
+  }, []);
 
   return null;
 };
